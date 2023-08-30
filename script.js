73212ba26988d37e0f463d3a2d76a9bd3c1f73cf
Nguyễn Trung Nghĩa <87295854+trngbro@users.noreply.github.com>
2023-08-30 14:14:00 +0700

    v2

--- script.js
+++ script.js
@@ -1,44 +1,59 @@
-let sidebar = document.querySelector(".sidebar");
-let closeBtn = document.querySelector("#btn");
-let searchBtn = document.querySelector(".bx-search");
+redirect = function () {
+  var queryParams = new URLSearchParams(window.location.search);
+  var redirect = queryParams.get('query');
+  return redirect ? redirect : error();
+};
 
-closeBtn.addEventListener("click", ()=>{
-  sidebar.classList.toggle("open");
-  menuBtnChange();//calling the function(optional)
-});
+function error(message) {
+  count = -1;
+  document.getElementById("head_noticed").innerHTML = "Xin lỗi";
+  document.getElementById("body_noticed").innerHTML = message ? message : "Không tồn tại đường dẫn";
+};
 
-searchBtn.addEventListener("click", ()=>{ // Sidebar open when you click on the search iocn
-  sidebar.classList.toggle("open");
-  menuBtnChange(); //calling the function(optional)
-});
-
-// following are the code to change sidebar button(optional)
-function menuBtnChange() {
- if(sidebar.classList.contains("open")){
-   closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
- }else {
-   closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
- }
+function countDown() {
+  var timer = document.getElementById("timer");
+  if (count > 0) {
+      count--;
+      timer.innerHTML = "Trang đang chuyển hướng sau " + count + " giây"; // Timer Message
+      setTimeout(countDown, 1000);
+  } else {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', 'data.json', true);
+      xhr.onload = function () {
+          if (xhr.status === 200) {
+              var data = JSON.parse(xhr.responseText);
+              var item = data.find(function (item) {
+                  return item.id === redirect;
+              });
+              console.log(item.name)
+              if (item) {
+                  if (item.type == 'link') {
+                      // Redirect to the URL
+                      window.location.href = item.url;
+                      console.log('Success to redirect');
+                  } else if (item.type == 'file') {
+                      document.open();
+                      document.write('');
+                      document.close();
+                      var embeddedContent = '<title> ' + (item.name) + ' </title><iframe src="' + item.url + '" width="100%" height="100%"></iframe>';
+                      document.write(embeddedContent);
+                      document.close();
+                      console.log('Success to opening');
+                  } else {
+                      error("Liên kết không khả dụng ngay bây giờ");
+                      console.log("Do not support");
+                  }
+              } else {
+                  // Handle error or redirect to default page
+                  console.log('Invalid query');
+              }
+          } else {
+              console.log('Failed to load data');
+          }
+      };
+      xhr.send();
+  }
 }
 
-$(document).ready(function(){
-  // Lấy giá trị của tham số 'link' từ URL
-  const urlParams = new URLSearchParams(window.location.search);
-  const link = urlParams.get('link');
-  
-  // Nếu có giá trị tham số 'link', gửi yêu cầu AJAX để tải ảnh lên
-  if (link !== null) {
-      $.ajax({
-          url: `img/${link}.png`,
-          type: 'GET',
-          dataType: 'text',
-          success: function(data){
-              // Đổi đường dẫn của thẻ <img> thành đường dẫn của ảnh tải về
-              $('#img-display').attr('src', `img/${link}.png`);
-          },
-          error: function(){
-              console.log('Failed to load image');
-          }
-      });
-  }
-});
+var count = 6; // Timer
+var redirect = redirect(); // Target URL
